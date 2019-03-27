93c086e218480197210b41cbf27f2fb2fbff590a
jakubgania <jakubgania@outlook.com>
2019-03-27 23:17:21 +0100

    updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,69 +2,49 @@ import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
 import pkg from './package'
 
 export default {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      }
-    ]
-  },
+	head: {
+		title: pkg.name,
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: pkg.description }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				rel: 'stylesheet',
+				href:
+					'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+			}
+		]
+	},
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+	loading: { color: '#fff' },
 
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/style/app.styl'
-  ],
+	css: [
+		'~/assets/style/app.styl'
+	],
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/vuetify'
-  ],
+	plugins: [
+		'@/plugins/vuetify'
+	],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/pwa',
-  ],
+	modules: [
+		'@nuxtjs/pwa',
+	],
 
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: ['vuetify/lib'],
-    plugins: [new VuetifyLoaderPlugin()],
-    loaders: {
-      stylus: {
-        import: ['~assets/style/variables.styl']
-      }
-    },
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
+	build: {
+		transpile: ['vuetify/lib'],
+		plugins: [new VuetifyLoaderPlugin()],
+		loaders: {
+			stylus: {
+				import: ['~assets/style/variables.styl']
+			}
+		},
+		
+		extend(config, ctx) {
+		}
+	}
 }
